d742e87da083b0adfa2ad000e9887d00f8fbe330
James <thejamesash1@gmail.com>
2021-03-17 12:34:47 -0400

    export birthdaydate

--- src/database/structures/Birthday.ts
+++ src/database/structures/Birthday.ts
@@ -1,5 +1,5 @@
 
-class BirthdayDate {
+export class BirthdayDate {
     day: string;
     month: string;
     year: string;
@@ -16,7 +16,7 @@ class BirthdayDate {
     }
 
     getYMD() {
-        
+
     }
 }
 
