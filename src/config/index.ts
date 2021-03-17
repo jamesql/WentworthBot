05f9718ebdc8f421ed02da2f7ee3bcefb520c5ed
James <thejamesash1@gmail.com>
2021-03-17 10:57:33 -0400

    add config stuff

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,20 @@
-export default null;
+
+
+const cfg = {
+
+    database: {
+        ip: "localhost",
+        port: "3306",
+        username: "afe",
+        password: "password",
+        database: "wt"
+    },
+
+    bot: {
+        token: "",
+        prefix: "",
+    }
+
+};
+
+export default cfg;
