118b1a1aadcfe197f81e9a06e45af56e079d8f9e
James <thejamesash1@gmail.com>
2021-03-18 17:46:13 -0400

    add types and start static db object

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,8 +4,19 @@ import {
     RoleLink
 } from "./structures";
 import mysql, { Connection, Query } from "mysql";
-
+import config from "../config";
 
 export default class Database {
+    static db: Connection;
+    constructor() {
+        if (!Database.db)
+            Database.connect();
+    }
+
+    static connect() {
+        const cfg = config.database;
+
+        Database.db = mysql.createConnection
+    }
 
 }
